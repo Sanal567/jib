fd1aee86ec128140d832a82a46b79df0e120df4e
Sunil Nalluru <sunil_nalluru@outlook.in>
2021-08-10 12:55:29 +0530

    Initial commit

---
+++ src/main/java/com/sanal/JLib/JLibDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sanal.JLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JLibDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JLibDemoApplication.class, args);
+	}
+
+}
